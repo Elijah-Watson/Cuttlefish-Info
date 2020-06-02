350a9c0530a594d28d8fb3ed24d734c9b6031291
Elijah Watson <ElijahLWatson@Gmail.com>
2020-06-02 01:47:34 -0400

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 module.exports = {
+	siteMetadata: {
+		siteUrl: 'https://cuttlefish-info.com',
+	},
 	plugins: [
 		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap'
 	],
 }
